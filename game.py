4ff6e025d42fa6704ba4876a84149165d1fff35d
Mikael MOREAU <mikaelmoreau@yahoo.fr>
2021-01-31 12:46:33 +0100

    srtuctured the code

---
+++ game.py
@@ -0,0 +1,9 @@
+import pygame
+from player import Player
+
+
+# Créer une seconde classe représentant notre jeu
+class Game:
+    def __init__(self):
+        # Generer le jouer
+        self.player = Player()
--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 import pygame
+from game import Game
 pygame.init()
-
-# Create the player class
-class Player(pygame.sprite.Sprite):
-  def __init__(self):
-    super().__init__()
-    self.health = 100
-    self.max_health = 100
-
-    self.attack = 10
-    self.velocity = 5
-
-    self.image = pygame.image.load('assets/player.png')
-    self.rect = self.image.get_rect()
-
 
 # Generer la fenetre du jeu
 pygame.display.set_caption('Commet Fall Game')
@@ -22,25 +9,25 @@ screen = pygame.display.set_mode((1080, 720))
 # importer l'arriere plan du jeu
 background = pygame.image.load('assets/bg.jpg')
 
-# Load the Player
-player = Player()
+# Charger le jeu
+game = Game()
 
 running = True
 
 # Boucle tant que cette condition est vrai
 while running:
 
-  # appliquer l'arriere plan
-  screen.blit(background, (0, -200))
+    # appliquer l'arriere plan
+    screen.blit(background, (0, -200))
 
-  # Add player image
-  screen.blit(player.image, player.rect)    
+    # Add player image
+    screen.blit(game.player.image, game.player.rect)
 
-  # mise à jour de l'ecran
-  pygame.display.flip()
+    # mise à jour de l'ecran
+    pygame.display.flip()
 
-  # si le joueur ferme la fenetre
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-      pygame.quit()
+    # si le joueur ferme la fenetre
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
